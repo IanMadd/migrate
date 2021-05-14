53a5cf4e4091234fce740f073347755d878c443c
Ian Maddaus <Ian.Maddaus@progress.com>
2021-05-14 12:24:25 -0700

    update main
    Signed-off-by: Ian Maddaus <Ian.Maddaus@progress.com>

--- main.py
+++ main.py
@@ -1,48 +1,13 @@
-import os
-import re
-import yaml
-import toml
 from pathlib import Path
 from support import syntax
 from support import examples
+from support import git
 
 
+repoList = ['../../inspec-aws/', '../../inspec-azure/', '../../inspec-habitat/']
 
-directory = Path("../inspec-aws/docs/resources")
-outputDirectory = Path("../inspec-aws/docs-chef-io/content/inspec/resources")
+inputFilePath = "docs/resources/"
+outputFilePath = "docs-chef-io/resources"
 
-
-
-
-
-def propertiesBlock(text):
-  blockHeadingText = "Properties"
-
-def matchersBlock(text):
-  blockHeadingText = "Matchers"
-
-inputSyntaxBlock = """
-Ensure that an `aws_alb` exists
-
-    describe aws_alb('arn:aws:elasticloadbalancing') do
-      it { should exist }
-    end
-
-    describe aws_alb(load_balancer_arn: 'arn:aws:elasticloadbalancing') do
-      it { should exist }
-    end
-    
-#### Parameters
-
-##### load\_balancer\_arn _(required)_
-
-This resource accepts a single parameter, the ALB Arn which uniquely identifies the ALB. 
-This can be passed either as a string or as a `load_balancer_arn: 'value'` key-value entry in a hash.
-More text here.
-
-See also the [AWS documentation on Elastic Load Balancing](https://docs.aws.amazon.com/elasticloadbalancing/latest/APIReference).
-
-"""
-
-output = syntax.mungeSyntaxBlock(inputSyntaxBlock, 0, 617)
-print(output["text"])
+for repo in repoList:
+  git.pullRepo(repo)
