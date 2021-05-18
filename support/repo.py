2c72abfca9eb2551b87e7d25ba567d0f986037ca
Ian Maddaus <Ian.Maddaus@progress.com>
2021-05-17 17:15:08 -0700

    rename git.py --> repo.py
    Signed-off-by: Ian Maddaus <Ian.Maddaus@progress.com>

--- support/repo.py
+++ support/repo.py
@@ -3,11 +3,15 @@ import git
 
 def pullRepo(file_path):
   repo = git.Repo(file_path)
+  repo.git.reset('--hard')
+  repo.git.clean('-fd')
   repo.git.checkout('master')
   repo.remotes.origin.pull('--ff')
 
 
 def newBranch(file_path, branch):
   repo = git.Repo(file_path)
+  if branch in repo.branches:
+    repo.delete_head(branch)
   repo.git.branch(branch)
   repo.git.checkout(branch)
