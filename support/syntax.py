9d44c0c3f5ad5e021b8dcfe3ca1b88d0785f548e
Ian Maddaus <Ian.Maddaus@progress.com>
2021-05-20 12:30:32 -0700

    Update syntax munging
    Signed-off-by: Ian Maddaus <Ian.Maddaus@progress.com>

--- support/syntax.py
+++ support/syntax.py
@@ -6,24 +6,26 @@ def mungeSyntaxBlock(text, start, end):
   regexParameters = r"^#+ Parameters"
   notHeadingNotNewline = r"^[^#|\n]"
   newLine = r"^\n"
-  syntaxText = misc.removeSlash(syntaxText)
 
   textList = syntaxText.splitlines(True)
 
   foundParameters = False
+  foundFirstParameter = False
   wrapLinesCount = 0
   for index, line in enumerate(textList):
-    print(line)
+    # print(line)
+    regexParameter = r"^#+ (\w+)"
     if foundParameters:
-      regexParameter = r"^#+ (\w+)"
       substParameter = "`\\g<1>`"
       regexEmptyLine = r"^ *\n"
       regexNormalLine = r"^"
       substNewLine = ": "
       substWrapLine = "  "
+      seeAlsoTheLineRegex = r"^See also the "
 
       if re.search(regexParameter, line):
         line = re.sub(regexParameter, substParameter, line, 0, re.M)
+        foundFirstParameter = True
         # print("this is the output: ")
         # print(output)
         # textList[index] = output
@@ -31,9 +33,13 @@ def mungeSyntaxBlock(text, start, end):
 
         regexRequired = r"_\(required\)_"
         substRequired = "| `REQUIRED`"
-        textList[index] = re.sub(regexRequired, substRequired, line, 0)
+        line = re.sub(regexRequired, substRequired, line, 0)
 
-      else:
+        regexOptional = r"_\(optional\)_"
+        substOptional = "| `OPTIONAL`"
+        textList[index] = re.sub(regexOptional, substOptional, line, 0)
+
+      elif foundFirstParameter:
         if re.search(regexEmptyLine, line):
           wrapLinesCount = 0
         else:
@@ -41,13 +47,14 @@ def mungeSyntaxBlock(text, start, end):
 
         if wrapLinesCount == 0:
           continue
-        elif wrapLinesCount == 1:
+        elif wrapLinesCount == 1 and not re.search(seeAlsoTheLineRegex, line):
           textList[index] = re.sub(regexNormalLine, substNewLine, line, 1, re.M)
         else:
-          textList[index] = re.sub(regexNormalLine, substWrapLine, line, 1, re.M)
+          if not re.search(seeAlsoTheLineRegex, line):
+            textList[index] = re.sub(regexNormalLine, substWrapLine, line, 1, re.M)
 
     if re.search(regexParameters, line):
-      print("found parameters:")
+      # print("found parameters:")
       foundParameters = True
 
   syntaxText = "".join(textList)
@@ -55,7 +62,7 @@ def mungeSyntaxBlock(text, start, end):
   substParameters = "where:"
   syntaxText = re.sub(regexParameters, substParameters, syntaxText, 0, re.M)
 
-  text = syntaxText[:start] + syntaxText + syntaxText[end:]
+  text = text[:start] + syntaxText + text[end:]
   return text
 
 def openSyntaxBlock(text):
--- tests/syntax.py
+++ tests/syntax.py
@@ -1,5 +1,5 @@
 import pytest
-from inspec.support.misc import mungeSyntaxBlock
+from inspec.support.syntax import mungeSyntaxBlock
 
 inputSyntaxBlock = """
 Ensure that an `aws_alb` exists
@@ -14,7 +14,7 @@ Ensure that an `aws_alb` exists
     
 #### Parameters
 
-##### load\_balancer\_arn _(required)_
+##### load_balancer_arn _(required)_
 
 This resource accepts a single parameter, the ALB Arn which uniquely identifies the ALB. 
 This can be passed either as a string or as a `load_balancer_arn: 'value'` key-value entry in a hash.
@@ -24,7 +24,6 @@ See also the [AWS documentation on Elastic Load Balancing](https://docs.aws.amaz
 """
 
 outputSyntaxBlock = """
-
 Ensure that an `aws_alb` exists
 
     describe aws_alb('arn:aws:elasticloadbalancing') do
@@ -37,23 +36,109 @@ Ensure that an `aws_alb` exists
     
 where:
 
-`load\_balancer\_arn` | `REQUIRED`
+`load_balancer_arn` | `REQUIRED`
 
 : This resource accepts a single parameter, the ALB Arn which uniquely identifies the ALB. 
   This can be passed either as a string or as a `load_balancer_arn: 'value'` key-value entry in a hash.
 
-: See also the [AWS documentation on Elastic Load Balancing](https://docs.aws.amazon.com/elasticloadbalancing/latest/APIReference).
+See also the [AWS documentation on Elastic Load Balancing](https://docs.aws.amazon.com/elasticloadbalancing/latest/APIReference).
 
 """
 
-
-
-
-
 def testMungeSyntaxBlock():
-  assert mungeSyntaxBlock(inputSyntaxBlock, 0, 619) == {"text": outputSyntaxBlock, "start": 0, "end": 619}
+  assert mungeSyntaxBlock(inputSyntaxBlock, 0, 619) == outputSyntaxBlock
 
-output = mungeSyntaxBlock(inputSyntaxBlock, 0, 619)
-print(output["text"])
+syntaxBlock2 = '''## Syntax
 
-print(outputSyntaxBlock)
+    describe aws_security_group('sg-12345678') do
+      it { should exist }
+    end
+
+    # May also use hash syntax
+    describe aws_security_group(group_id: 'sg-12345678') do
+      it { should exist }
+    end
+
+    # Ensure you have a Security Group with a specific name.  Names are
+    # unique within a VPC but not across VPCs.
+    # Using only Group returns an error if multiple SGs match.
+    describe aws_security_group(group_name: 'my-group') do
+      it { should exist }
+    end
+
+    # Add vpc_id to ensure uniqueness.
+    describe aws_security_group(group_name: 'my-group', vpc_id: 'vpc-12345678') do
+      it { should exist }
+    end
+
+#### Parameters
+
+You must provide at least one parameter; `group_id`, `group_name` or `vpc_id`
+
+##### group_id _(required if no other parameter provided)_
+
+The Security Group ID which uniquely identifies the SG.
+This can be passed either as a string or as a `group_id: 'value'` key-value entry in a hash.
+
+##### group_name _(required if no other parameter provided)_
+
+The Security Group name.
+This can be passed either as a string or as a `group_name: 'value'` key-value entry in a hash.
+
+##### vpc_id _(required if no other parameter provided)_
+
+The ID of the VPC associated with the SG.
+This can be passed either as a string or as a `vpc_id: 'value'` key-value entry in a hash.
+
+See also the [AWS documentation on Security Groups](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-network-security.html).
+'''
+
+
+syntaxBlock2Output = '''## Syntax
+
+    describe aws_security_group('sg-12345678') do
+      it { should exist }
+    end
+
+    # May also use hash syntax
+    describe aws_security_group(group_id: 'sg-12345678') do
+      it { should exist }
+    end
+
+    # Ensure you have a Security Group with a specific name.  Names are
+    # unique within a VPC but not across VPCs.
+    # Using only Group returns an error if multiple SGs match.
+    describe aws_security_group(group_name: 'my-group') do
+      it { should exist }
+    end
+
+    # Add vpc_id to ensure uniqueness.
+    describe aws_security_group(group_name: 'my-group', vpc_id: 'vpc-12345678') do
+      it { should exist }
+    end
+
+where:
+
+You must provide at least one parameter; `group_id`, `group_name` or `vpc_id`
+
+`group_id` _(required if no other parameter provided)_
+
+: The Security Group ID which uniquely identifies the SG.
+  This can be passed either as a string or as a `group_id: 'value'` key-value entry in a hash.
+
+`group_name` _(required if no other parameter provided)_
+
+: The Security Group name.
+  This can be passed either as a string or as a `group_name: 'value'` key-value entry in a hash.
+
+`vpc_id` _(required if no other parameter provided)_
+
+: The ID of the VPC associated with the SG.
+  This can be passed either as a string or as a `vpc_id: 'value'` key-value entry in a hash.
+
+See also the [AWS documentation on Security Groups](https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-network-security.html).
+'''
+
+
+def testAnotherSyntaxBlock():
+  assert mungeSyntaxBlock(syntaxBlock2, 0, 1463) == syntaxBlock2Output
