53ba61534da1ea1fba001c1685f75a5bbd9e0791
Ian Maddaus <ian.maddaus@progress.com>
2021-12-10 14:48:57 -0500

    Fix indentation, handle definition with 3rd key/value pair that's not field
    Signed-off-by: Ian Maddaus <ian.maddaus@progress.com>

--- munge/dictToDefinition.py
+++ munge/dictToDefinition.py
@@ -1,27 +1,29 @@
 import re
 
 def convertDictToDef(dict):
-  definitionsMarkdown = ''
+    definitionsMarkdown = ''
 
-  for term in dict:
-    for key, value in term.items():
-      if key == "property":
-        definitionsMarkdown += '`' + value + '`\n'
-      elif key == "description":
-        if not value.endswith('.'):
-          value += '.'
-        definitionsMarkdown += ": " + value + '\n'
-      elif key == 'field':
-        definitionsMarkdown += "\n: **Field**: " + value + '\n'
+    for term in dict:
+        for key, value in term.items():
+            if key == "property":
+                definitionsMarkdown += '`' + value + '`\n'
+            elif key == "description":
+                if not value.endswith('.'):
+                    value += '.'
+                definitionsMarkdown += ": " + value + '\n'
+            elif key == 'field':
+                definitionsMarkdown += "\n: **Field**: " + value + '\n'
+            else:
+                definitionsMarkdown += "\n: **" + key.title() + "**: " + value + '\n'
 
-    definitionsMarkdown += "\n"
+        definitionsMarkdown += "\n"
 
-  return definitionsMarkdown.strip()
+    return definitionsMarkdown.strip()
 
 if __name__ == '__main__':
-  dict1 = [{'property': 'bucket_name', 'description': 'The name of the bucket.'},
-  {'property': 'key', 'description': 'The key within the bucket.'},
-  {'property': 'content_length', 'description': 'Size of the body in bytes.'},
-  ]
+    dict1 = [{'property': 'bucket_name', 'description': 'The name of the bucket.'},
+    {'property': 'key', 'description': 'The key within the bucket.'},
+    {'property': 'content_length', 'description': 'Size of the body in bytes.'},
+    ]
 
-  print(convertDictToDef(dict1))
+    print(convertDictToDef(dict1))
